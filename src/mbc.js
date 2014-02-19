5dd3140716e201983827f410c8364a5f8c674fd4
Scott Burch <scott@bulldoginfo.com>
2014-02-19 00:20:28 -0800

    adding MBC stuff

--- src/mbc.js
+++ src/mbc.js
@@ -1,9 +1,10 @@
 (function (global) {
-    global.MBC = {};
+    var MBC = global.MBC = {};
 
-    global.MBC.renderTemplate = function (templateName, elOrSelector) {
+    MBC.renderTemplate = function (templateName, elOrSelector) {
         var el = _.isString(elOrSelector) ? $(elOrSelector).get(0) : elOrSelector;
         el && UI.DomRange.insert(UI.render(Template[templateName]).dom, el);
     };
+
 
 }(this));
