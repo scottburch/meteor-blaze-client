40ad20385a0487622a7da615526e1536d6f8114b
Scott Burch <scott@bulldoginfo.com>
2014-02-18 23:03:27 -0800

    build script should work

--- scripts/build.js
+++ scripts/build.js
@@ -14,7 +14,12 @@ remakeDir(buildDir);
 createMeteorProject()
     .then(bundleProject)
     .then(unpackBundle)
-    .then(copyMeteorFiles)
+    .then(createJS)
+    .then(removeBuildDir)
+
+function removeBuildDir() {
+    rmDirRecursive(buildDir);
+}
 
 function unpackBundle() {
     console.log('unpacking bundle');
@@ -33,11 +38,73 @@ function bundleProject() {
     return execute(meteorCmd + ' bundle temp.tar.gz --debug', {cwd: normalize('dist/build/meteorProject')});
 }
 
-function copyMeteorFiles(error, stdout, stderr) {
-    console.log('copying meteor files');
-    var files = [
+function createJS() {
+    console.log('creating JS file');
+    var packageFiles = [
+            "underscore.js",
+            "meteor.js",
+            "json.js",
+            "ejson.js",
+            "logging.js",
+            "reload.js",
+            "deps.js",
+            "random.js",
+            "retry.js",
+            "check.js",
+            "ordered-dict.js",
+            "geojson-utils.js",
+            "minimongo.js",
+            "application-configuration.js",
+            "standard-app-packages.js",
+            "webapp.js",
+            "reactive-dict.js",
+            "session.js",
+            "jquery.js",
+            "observe-sequence.js",
+            "htmljs.js",
+            "ui.js",
+            "spacebars.js",
+            "templating.js",
+            "global-imports.js"
+        ];
+    
+    var compilerFiles = [
+            "html-tools/parse.js",
+            "html-tools/scanner.js",
+            "html-tools/charref.js",
 
-    ]
+            "html-tools/tokenize.js",
+            "html-tools/parse.js",
+            "html-tools/exports.js",
+
+            "templating/plugin/html_scanner.js",
+
+
+            "spacebars-compiler/templatetag.js",
+            "spacebars-compiler/tojs.js",
+            "spacebars-compiler/tokens.js",
+            "spacebars-compiler/spacebars-compiler.js"
+    ];
+
+    var out = '';
+
+    out = packageFiles.reduce(function(out, filename) {
+        var packageDir = normalize('dist/build/meteorProject/bundle/programs/client/packages');
+        out = out + '\n\n//--------- ' + filename + '-----------\n';
+        out = out + fs.readFileSync(packageDir + '/' + filename, {encoding:'UTF-8'});
+        return out;
+    }, out);
+
+    out = compilerFiles.reduce(function(out, filename) {
+        var packageDir = normalize('vendor/meteor/packages');
+        out = out + '\n\n//-------------' + filename + '----------\n';
+        out = out + fs.readFileSync(packageDir + '/' + filename, {encoding:'UTF-8'});
+        return out;
+    }, out);
+
+    fs.writeFileSync(normalize('dist/meteor-blaze-client.js'), out);
+
+
 }
 
 function execute(command, options, cb) {
@@ -58,23 +125,24 @@ function normalize(filename) {
 function remakeDir(dir) {
     rmDirRecursive(dir);
     fs.mkdirSync(dir);
-    function rmDirRecursive(path) {
-        var files = [];
-        if( fs.existsSync(path) ) {
-            files = fs.readdirSync(path);
-            files.forEach(function(file,index){
-                var curPath = path + "/" + file;
-                if(fs.statSync(curPath).isDirectory()) { // recurse
-                    rmDirRecursive(curPath);
-                } else { // delete file
-                    fs.unlinkSync(curPath);
-                }
-            });
-            fs.rmdirSync(path);
-        }
-    };
 
 }
+
+function rmDirRecursive(path) {
+    var files = [];
+    if( fs.existsSync(path) ) {
+        files = fs.readdirSync(path);
+        files.forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+                rmDirRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
 
 
 function error(message) {
@@ -103,7 +171,7 @@ var files = [
     'mongo-livedata/collection.js',
     'mongo-livedata/local_collection_driver.js',
 
-    'random/random.js',
+    'random/random.js'
 
 ];
 
